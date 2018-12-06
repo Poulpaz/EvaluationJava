814ce3672e5aed6f6ee12f87f93c0711b75e13fc
Tristan <letristandu39@hotmail.fr>
2018-12-06 17:46:06 +0100

    started just with jeu

---
+++ src/main/fr/tennismatch/JeuTennis.java
@@ -0,0 +1,10 @@
+package fr.tennismatch;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JeuTennis {
+
+    private Player winner;
+    private List<String> scoreJeu = new ArrayList<String>();
+}
--- src/main/fr/tennismatch/TennisMatch.java
+++ src/main/fr/tennismatch/TennisMatch.java
@@ -5,12 +5,40 @@ public class TennisMatch {
     private MatchType matchType;
     private Player player1;
     private Player player2;
-    private Boolean tieBreak;
+    private Boolean tieBreakInLastSet;
+    private JeuTennis jeuTennis;
 
-    public TennisMatch(MatchType matchType, Player player1, Player player2, Boolean tieBreak) {
+    public TennisMatch(MatchType matchType, Player player1, Player player2, Boolean tieBreakInLastSet, JeuTennis jeuTennis) {
         this.matchType = matchType;
         this.player1 = player1;
         this.player2 = player2;
-        this.tieBreak = tieBreak;
+        this.tieBreakInLastSet = tieBreakInLastSet;
+        this.jeuTennis = jeuTennis;
+    }
+
+    public void updateWithPointWonBy(Player player) {
+        if(jeuTennis != null) {
+            player.
+        } else { return; }
+    }
+
+    public String pointsForPlayer(Player player) {
+
+    }
+
+    public int currentSetNumber() {
+
+    }
+
+    public int gamesInCurrentSetForPlayer(Player player) {
+
+    }
+
+    public int gamesInSetForPlayer(int setNumber, Player player) {
+
+    }
+
+    public boolean isFinished() {
+
     }
 }
